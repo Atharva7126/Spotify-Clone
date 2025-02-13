7e22421389d8cabf1a2f72995b5755a1ca9a1cfa
Atharva7126 <atharvaraut7126@gmail.com>
2025-02-13 13:50:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function convertSecondsToMinSec(seconds) {
 
 async function GetSongs(folder) {
     currfolder = folder
-    let a = await fetch(`songs/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text()
 
     let div = document.createElement("div")
